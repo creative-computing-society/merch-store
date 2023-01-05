92585bb512eda9e2c37a636a800a622f13872f39
Arvinder Singh Kandola <askandola2@gmail.com>
2023-01-05 22:16:32 +0530

    fix change-password bug

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view()),
-    path('change-password/', views.LoginView.as_view()),
+    path('change-password/', views.ChangePassword.as_view()),
     path('logout/', views.LogoutView.as_view()),
     path('user/', views.UserDetails.as_view())
 ]
