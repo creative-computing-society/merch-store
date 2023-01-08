a70de5791df2a47fe25afcc4f9143b280f2cc79a
Arvinder Singh Kandola <askandola2@gmail.com>
2023-01-09 01:45:18 +0530

    try to prevent duplicate instance of background jobs

--- jobs/sched.py
+++ jobs/sched.py
@@ -1,8 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 from .job import send_mails
 
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler(jobstores={'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')})
 
 def start():
     scheduler.add_job(send_mails, 'interval', minutes=1, replace_existing=True, id='send_mails', name='send_mails')
