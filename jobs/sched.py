9fb0f7cd012ecce7c1f307aa79363252bf86d3b1
Arvinder Singh Kandola <askandola2@gmail.com>
2023-01-09 01:50:49 +0530

    reset background scheduler time interval

--- jobs/sched.py
+++ jobs/sched.py
@@ -6,5 +6,5 @@ from .job import send_mails
 scheduler = BackgroundScheduler(jobstores={'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')})
 
 def start():
-    scheduler.add_job(send_mails, 'interval', minutes=1, replace_existing=True, id='send_mails', name='send_mails')
+    scheduler.add_job(send_mails, 'interval', minutes=15, replace_existing=True, id='send_mails', name='send_mails')
     scheduler.start()
