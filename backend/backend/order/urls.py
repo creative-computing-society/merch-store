da2ef5bb63d7d5c9a6fb3216a30655d8a383d1d8
Pancham Agarwal <pancham1603@gmail.com>
2024-08-18 02:28:58 +0530

    fulfilled cf requirements

--- backend/backend/order/urls.py
+++ backend/backend/order/urls.py
@@ -4,10 +4,10 @@ from .views import *
 
 urlpatterns = [
     path("order/all/", AllOrders.as_view(), name="all_orders"),
-    path("order/<str:order_id>/", OrderView.as_view(), name="order_view"),
     path("order/place/", Checkout.as_view(), name="place_order"),
     path("order/apply-discount/", ApplyDiscount.as_view(), name="apply_discount"),
     path('payment/<int:order_id>/', PaymentView.as_view(), name='payment_checkout'),
-    path('payment/webhook/<str:order_id>/', PaymentWebhookView.as_view(), name='payment_webhook'),
     path('payment/verify/', PaymentVerifyView.as_view(), name='payment_verfiy'),
+    path("order/<str:order_id>/", OrderView.as_view(), name="order_view"),
+    path('payment/webhook/<str:order_id>/', PaymentWebhookView.as_view(), name='payment_webhook'),
 ]
